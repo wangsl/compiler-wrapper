49b2dec90e72e033ed8657b9637da6df2885a060
wangsl2001@gmail.com <wangsl2001@gmail.com@1b6eb556-f4a1-b5e9-ea2e-34486d17df67>
2011-12-06 17:06:46 +0000

    Import wrapper, Tue Dec  6 12:05:55 EST 2011

---
+++ abspath.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+function absolute_path()
+{
+    if [ ! -e $1 ]; then
+	echo "'$1' does not exist"
+	exit 1
+    fi
+    
+    local pwd_=$(pwd)
+    
+    if [ -d "$1" ]; then
+	cd "$1"
+	pwd
+    else
+	local base_name=$(basename "$1")
+	local path=$(dirname "$1")
+	path=$(cd $path; pwd)
+	if [ "$path" == "/" ]; then
+	    echo "/$base_name" 
+	else
+	    echo "$path/$base_name" 
+	fi
+    fi
+
+    cd ${pwd_}
+}
+
+if [ $# -eq 0 ]; then
+    pwd
+else
+    for file in $*; do
+	absolute_path $file
+    done
+fi
+
+exit 0
+
---
+++ build.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+#export BUILD_WRAPPER_SCRIPT=
+#export SPECIAL_RULES_FUNCTION=
+
+#export SOURCE_CODE_LIST_WITH_INTEL_COMPILERS=
+#export SOURCE_CODE_LIST_WITH_GNU_COMPILERS=
+#export REGULAR_EXPRESSION_LIST_FOR_SOURCE_CODE_WITH_INTEL_COMPILERS=
+#export REGULAR_EXPRESSION_LIST_FOR_SOURCE_CODE_WITH_GNU_COMPILERS=
+#export INVALID_FLAGS_FOR_INTEL_COMPILERS="-O2 -g"
+#export INVALID_FLAGS_FOR_GNU_COMPILERS=
+#export OPTIMIZATION_FLAGS=
+#export OPTIMIZATION_FLAGS_FOR_INTEL_COMPILERS="-O3 -fPIC -unroll -ip -axOP -xOP -openmp -vec-report -par-report -openmp-report -Wno-deprecated -shared-intel -Wp64"
+#export OPTIMIZATION_FLAGS_FOR_INTEL_FORTRAN_COMPILERS=
+#export OPTIMIZATION_FLAGS_FOR_GNU_COMPILERS=
+#export OPTIMIZATION_FLAGS_FOR_GNU_FORTRAN_COMPILERS=
+#export INCLUDE_FLAGS=
+#export INCLUDE_FLAGS_FOR_INTEL_COMPILERS=
+#export INCLUDE_FLAGS_FOR_INTEL_FORTRAN_COMPILERS=
+#export INCLUDE_FLAGS_FOR_GNU_COMPILERS=
+#export INCLUDE_FLAGS_FOR_GNU_FORTRAN_COMPILERS=
+#export LINK_FLAGS=
+#export LINK_FLAGS_FOR_INTEL_COMPILERS="
+#export LINK_FLAGS_FOR_GNU_COMPILERS="-fopenmp"
+#export EXTRA_OBJECT_FILE_AND_LIBRARY_LIST=
+#export STRING_MACROS=
+#export FUNCTION_MACROS=
+#export INTEL_MPI_BIN_PATH=
+#export GNU_MPI_BIN_PATH=
+#export DEFAULT_COMPILER="INTEL"
+#export NO_ECHO_FLAGS=
+#export REGULAR_EXPRESSIONS_FOR_NO_ECHO=
+#export STRING_PREPEND_TO_ECHO=
+#export DEBUG_LOG_FILE=tmp.log
+
+#export CC=icc
+#export CFLAGS="-O3 -fPIC -unroll -ip -axOP -xOP -openmp -vec-report -par-report -openmp-report -Wno-deprecated -shared-intel"
+#export LDFLAGS="-shared-intel $CFLAGS"
+#export LIBS=
+#export CPPFLAGS=
+#export CPP="icc -E"
+#export CCAS=
+#export CCASFLAGS=
+#export CXX=icpc
+#export CXXFLAGS=
+#export CXXCPP="icpc -E"
+#export F77=ifort
+#export FFLAGS="$CFLAGS"
+
+function special_rules()
+{
+    return
+    local arg=
+    for arg in $*; do
+	echo $arg
+    done
+}
+
+function main() 
+{
+    source /etc/profile.d/env-modules.sh
+    module purge
+
+    #export BUILD_WRAPPER_SCRIPT=$(abspath.sh $0)
+    #export SPECIAL_RULES_FUNCTION=special_rules
+
+    export INTEL_BIN_PATH=$(dirname $(which icpc))
+    export GNU_BIN_PATH=$(dirname $(which g++))
+    
+    if [ "$DEBUG_LOG_FILE" != "" ]; then
+	rm -rf $DEBUG_LOG_FILE
+    fi
+
+    export LD_RUN_PATH=$LD_LIBRARY_PATH
+
+    local args=$*
+    local arg=
+    for arg in $args; do
+	
+	case $arg in
+
+	    configure|conf)
+		echo " Run configuration ..."
+		./configure --build=x86_64-redhat-linux \
+		    --prefix=/share/apps/flex/2.5.35/intel
+		shift
+		;;
+	    
+	    make)
+		export PATH=.:$HOME/bin/intel:$PATH
+		echo " Run make"
+		eval "$args" 
+		exit
+            ;;
+	    
+	    *)
+		echo " Usage: $0 <argument>: configure make"
+		exit 1
+		;;
+	esac
+
+	args=$(eval "echo $args | sed -e 's/$arg //'")
+    done
+}
+
+## do the main work here
+## do not modify the follwoing part, we just need to modify the main function
+
+if [ "$TO_SOURCE_BUILD_WRAPPER_SCRIPT" == "" ]; then
+    main "$*"
+    exit
+else
+    unset -f main
+fi
