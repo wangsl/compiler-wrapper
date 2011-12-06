1b431c83555f581413da60f43dba27ec7d7bf392
wangsl2001@gmail.com <wangsl2001@gmail.com@1b6eb556-f4a1-b5e9-ea2e-34486d17df67>
2011-12-06 17:06:46 +0000

    Import wrapper, Tue Dec  6 12:05:55 EST 2011

---
+++ trunk/link.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+compilers="cc c++ f77 g++ g77 gcc gfortran icc icpc ifort mpic++ mpicc mpiCC mpicxx mpif77 mpif90"
+
+for comp in $compilers;  do
+    echo $comp
+    ln -s intel-wrapper.sh $comp
+done
