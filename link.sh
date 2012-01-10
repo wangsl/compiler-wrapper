7cf36b46911ab8bbbe8b6c454f539913cf1eeb2f
wangsl2001@gmail.com <wangsl2001@gmail.com@1b6eb556-f4a1-b5e9-ea2e-34486d17df67>
2012-01-10 01:35:08 +0000

    add for PGI compilers

--- link.sh
+++ link.sh
@@ -2,7 +2,7 @@
 
 # $Id$
 
-compilers="cc c++ f77 g++ g77 gcc gfortran icc icpc ifort mpic++ mpicc mpiCC mpicxx mpif77 mpif90"
+compilers="cc c++ f77 g++ g77 gcc gfortran icc icpc ifort mpic++ mpicc mpiCC mpicxx mpif77 mpif90 pgf77 pgf90 pgfortran pgcc pgc++"
 
 for comp in $compilers;  do
     echo $comp
