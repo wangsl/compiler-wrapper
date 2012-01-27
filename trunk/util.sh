74754e509810ef175d28102e81142935b84d85f5
wangsl2001@gmail.com <wangsl2001@gmail.com@1b6eb556-f4a1-b5e9-ea2e-34486d17df67>
2012-01-27 17:21:59 +0000

    add util.sh

---
+++ trunk/util.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+# $Id:$
+
+function prepend_to_env_variable()
+{
+    local env_variable=$1
+    local new_arguments=$2
+    local seperate_field=" "
+    if [[ $# -eq 3 ]]; then
+	local seperate_field=$3
+    fi
+    local cmd="export $env_variable=\"${new_arguments}${seperate_field}\$${env_variable}${seperate_field}\""
+    eval "$cmd"
+    return
+}
+
+function append_to_env_variable()
+{
+    local env_variable=$1
+    local new_arguments=$2
+    local seperate_field=" "
+    if [[ $# -eq 3 ]]; then
+	local seperate_field=$3
+    fi
+    local cmd="export $env_variable=\"\$${env_variable}${seperate_field}${seperate_field}${new_arguments}\""
+    eval "$cmd"
+    return
+}
+
+function LD_LIBRARY_PATH_to_rpath()
+{
+    local ld_lib_paths=$(echo $LD_LIBRARY_PATH | sed -e "s/:/\n/g" | sort -u)
+    
+    local lib_path=
+    for lib_path in $ld_lib_paths; do
+	if [ "$lib_path" != "." ]; then
+	    if [ -d $lib_path ]; then
+		echo -n "-Wl,-rpath=$lib_path "
+	    fi
+	fi
+    done
+    echo
+}
+
+alias die='_error_exit_ "Error in file $0 at line $LINENO\n"'
+alias warn='_warn_ "Warn in file $0 at line $LINENO\n"'
+
+function cecho()
+{
+    local black='\E[30;47m'
+    local red='\E[31;47m'
+    local green='\E[32;47m'
+    local yellow='\E[33;47m'
+    local blue='\E[34;47m'
+    local magenta='\E[35;47m'
+    local cyan='\E[36;47m'
+    local white='\E[37;47m'
+    
+    local color=$1
+    local message="$2"
+    
+    local command="echo -en \$$color\$message"
+    eval $command
+    tput sgr0
+    echo
+}
+
+function _error_exit_()
+{
+    cecho "red" "$*"
+    echo
+    exit 1
+}
+
+function _warn_()
+{
+    cecho "cyan" "$*"
+    echo
+}
+
+function sort_and_uniq()
+{
+    if [ "$*" != "" ]; then 
+	echo "$*" | sed -e 's/ /\n/g' | sort -u
+    fi
+}
